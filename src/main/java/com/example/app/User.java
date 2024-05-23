da67e57154c7ab132c95084fd587ecce0117a919
Scotopia <johnnyjneid7@gmail.com>
2024-05-23 18:02:59 +0300

    v1

---
+++ src/main/java/com/example/app/User.java
@@ -0,0 +1,131 @@
+package com.example.app;
+
+import org.bson.Document;
+
+import java.util.ArrayList;
+
+public class User {
+    private String username;
+    private String password;
+    private String fonction;
+    private static ArrayList<User> Userlist = new ArrayList<User>();
+
+    public User() {
+
+    }
+
+    public User(String username, String password, String fonction) {
+        setUsername(username);
+        setPassword(password);
+        setFonction(fonction);
+        Userlist.add(this);
+    }
+
+    private void setUsername(String username) {
+        this.username = username.toLowerCase();
+    }
+
+    private void setPassword(String password) {
+        this.password = password.toLowerCase();
+    }
+
+    private void setFonction(String fonction) {
+        this.fonction = fonction;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFonction() {
+        return fonction;
+    }
+
+    public static ArrayList<User> getUserlist() {
+        return Userlist;
+    }
+
+    public static ArrayList<String> getUsernamelist() {
+        ArrayList<String> Usernamelist = new ArrayList<String>();
+        for (int i = 0; i < Userlist.size(); i++) {
+            Usernamelist.add(Userlist.get(i).getUsername());
+        }
+        return Usernamelist;
+    }
+
+    public static Boolean isAvailable(String username) {
+        for (int i = 0; i < Userlist.size(); i++) {
+            if (Userlist.get(i).getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String getPasswordbyuser(String username) {
+        for (int i = 0; i < Userlist.size(); i++) {
+            if (Userlist.get(i).getUsername().equals(username)) {
+                return Userlist.get(i).getPassword();
+            }
+        }
+        return "";
+    }
+
+    public static String getFonctionbyuser(String username) {
+        for (int i = 0; i < Userlist.size(); i++) {
+            if (Userlist.get(i).getUsername().equals(username)) {
+                return Userlist.get(i).getFonction();
+            }
+        }
+        return "";
+    }
+
+    public static void ChangePassword(String username, String password) {
+        for (int i = 0; i < Userlist.size(); i++) {
+            if (Userlist.get(i).getUsername().equals(username)) {
+                Userlist.get(i).setPassword(password);
+                DatabaseConnection.changePassword(username, password);
+            }
+        }
+    }
+
+    public static void ChangeFonction(String username, String fonction) {
+        for (int i = 0; i < Userlist.size(); i++) {
+            if (Userlist.get(i).getUsername().equals(username)) {
+                Userlist.get(i).setFonction(fonction);
+                DatabaseConnection.changeFonction(username, fonction);
+            }
+        }
+    }
+
+    public static void DeleteByFonction(String fonction) {
+        for (int i = 0; i < Userlist.size(); i++) {
+            if (Userlist.get(i).getFonction().equals(fonction)) {
+                User user = Userlist.get(i);
+                Userlist.remove(i);
+                DatabaseConnection.deleteUser(user.getUsername());
+            }
+        }
+    }
+
+    public static void deleteUser(String username) {
+        for (int i = 0; i < Userlist.size(); i++) {
+            if (Userlist.get(i).getUsername().equals(username)) {
+                Userlist.remove(i);
+                DatabaseConnection.deleteUser(username);
+            }
+        }
+    }
+
+    public static void printUserList() {
+        for (int i = 0; i < Userlist.size(); i++) {
+            System.out.println("{Username:" + Userlist.get(i).getUsername() +
+                    "; Password:" + Userlist.get(i).getPassword() +
+                    "; Fonction:" + Userlist.get(i).getFonction() + "}");
+        }
+    }
+}
---
+++ src/main/java/com/example/app/newOrder.java
@@ -0,0 +1,124 @@
+package com.example.app;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class newOrder {
+
+    @FXML
+    private Pane pane;
+
+    private static int buttonwidth = 150;
+    private static int buttonheight = 90;
+
+    @FXML
+    protected void initialize() {
+        System.out.println("New Order Screen Loaded " + Date.from(java.time.Instant.now()));
+        loadButtons(Category.getCategories());
+    }
+
+    @FXML
+    protected void btnBackOnAction() {
+
+        // When the "Cancel" button is pressed
+        System.out.println("Back Button Pressed " + Date.from(java.time.Instant.now()));
+        try {
+            // Load the PowerOff.fxml file
+            FXMLLoader fxmlLoader = new FXMLLoader(LoginScreen.class.getResource("HomeScreen.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+
+            // Create a new scene and stage for the "Power Off" window
+            Scene newScene = new Scene(root);
+            Stage newStage = new Stage();
+
+            newStage.initStyle(StageStyle.UNDECORATED);
+            newStage.setResizable(false);
+            newStage.setScene(newScene);
+            newStage.setTitle("Home Screen");
+
+            // Show the "Power Off" window
+            newStage.show();
+
+            Stage homeStage = (Stage) pane.getScene().getWindow();
+            homeStage.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void loadButtons(ArrayList<String> categories) {
+        for (String category : categories) {
+            Button button = new Button(category);
+            button.setOnAction(event -> {
+                pane.getChildren().clear();
+                System.out.println("Button Pressed: " + category + " " + Date.from(java.time.Instant.now()));
+                for (String item : MenuItem.getItems(category)) {
+                    Button itemButton = new Button(item);
+                    itemButton.setPrefSize(buttonwidth, buttonheight);
+                    itemButton.setStyle("-fx-background-color: #fcfcfc;-fx-border-color: #252525; " +
+                            "-fx-border-width: 5px; -fx-font-family: 'Times New Roman' ; -fx-font-size: 20px; -fx-font-weight: bold;");
+                    itemButton.setOnAction(e -> {
+                        System.out.println("Button Pressed: " + item + " " + Date.from(java.time.Instant.now()));
+                        pane.getChildren().clear();
+                        // Load the items ingredients
+                        for (String ingredient : DatabaseConnection.LoadIngredients(item)) {
+                            Button ingredientButton = new Button(ingredient);
+                            ingredientButton.setPrefSize(buttonwidth, buttonheight);
+                            ingredientButton.setStyle("-fx-background-color: #fcfcfc;-fx-border-color: #252525; " +
+                                    "-fx-border-width: 5px; -fx-font-family: 'Times New Roman' ; -fx-font-size: 20px; -fx-font-weight: bold;");
+                            pane.getChildren().add(ingredientButton);
+                            System.out.println("Ingredient: " + ingredient + "Loaded " + Date.from(java.time.Instant.now()));
+                        }
+                        Button backButton = new Button("Back");
+                        backButton.setOnAction(e2 -> {
+                            pane.getChildren().clear();
+                            loadButtons(Category.getCategories());
+                        });
+                        backButton.setPrefSize(buttonwidth, buttonheight);
+                        backButton.setStyle("-fx-background-color: #fcfcfc;-fx-border-color: #252525; " +
+                                "-fx-border-width: 5px; -fx-font-family: 'Times New Roman' ; -fx-font-size: 20px; -fx-font-weight: bold;");
+                        pane.getChildren().add(backButton);
+                    });
+                    pane.getChildren().add(itemButton);
+                    System.out.println("Item: " + item + "Loaded " + Date.from(java.time.Instant.now()));
+                }
+                Button backButton = new Button("Back");
+                backButton.setOnAction(e -> {
+                    pane.getChildren().clear();
+                    loadButtons(Category.getCategories());
+                });
+                backButton.setPrefSize(buttonwidth, buttonheight);
+                backButton.setStyle("-fx-background-color: #fcfcfc;-fx-border-color: #252525; " +
+                        "-fx-border-width: 5px; -fx-font-family: 'Times New Roman' ; -fx-font-size: 20px; -fx-font-weight: bold;");
+                pane.getChildren().add(backButton);
+            });
+            button.setPrefSize(buttonwidth, buttonheight);
+            button.setStyle("-fx-background-color: #fcfcfc;-fx-border-color: #252525; " +
+                    "-fx-border-width: 5px; -fx-font-family: 'Times New Roman' ; -fx-font-size: 20px; -fx-font-weight: bold;");
+            pane.getChildren().add(button);
+            System.out.println("Category: " + category + "Loaded " + Date.from(java.time.Instant.now()));
+        }
+        Button RefreshButton = new Button("Refresh");
+        RefreshButton.setOnAction(e -> {
+            pane.getChildren().clear();
+            loadButtons(Category.getCategories());
+        });
+        RefreshButton.setPrefSize(buttonwidth, buttonheight);
+        RefreshButton.setStyle("-fx-background-color: #fcfcfc;-fx-border-color: #252525; " +
+                "-fx-border-width: 5px; -fx-font-family: 'Times New Roman' ; -fx-font-size: 20px; -fx-font-weight: bold;");
+        pane.getChildren().add(RefreshButton);
+    }
+}
