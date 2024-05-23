bd5de0d592436a0fc5e33db34a60ee727f650394
Scotopia <johnnyjneid7@gmail.com>
2024-05-23 18:09:51 +0300

    update

--- src/main/java/com/example/app/LoginController.java
+++ src/main/java/com/example/app/LoginController.java
@@ -18,168 +18,158 @@ import java.util.Date;
 
 
 public class LoginController {
-    // FXML annotations to inject UI elements
-    @FXML
-    private Pane rootPane; // Make sure you have a reference to the root pane or scene
+	// FXML annotations to inject UI elements
+	@FXML
+	private Pane rootPane; // Make sure you have a reference to the root pane or scene
 
-    @FXML
-    private Button LoginButton;
+	@FXML
+	private Button LoginButton;
 
-    @FXML
-    private TextField UsernameField;
+	@FXML
+	private TextField UsernameField;
 
-    @FXML
-    private PasswordField PasswordField;
+	@FXML
+	private PasswordField PasswordField;
 
-    @FXML
-    private Label ErrLabel;
+	@FXML
+	private Label ErrLabel;
 
-    @FXML
-    public void initialize() {
-        setupKeyEvents();
-        // Set the focus on the username field when the window is loaded
-        UsernameField.requestFocus();
-        //make the login button click when the enter key is pressed
-        rootPane.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
-            if (key.getCode() == KeyCode.ENTER) {
-                LoginButton.fire();
-            } else if (key.getCode() == KeyCode.F11) {
-                FullScreen.toggleFullScreen();
-                System.out.println("Full Screen Toggled " + Date.from(new Date().toInstant()));
-            }
-        });
-    }
+	@FXML
+	public void initialize() {
+		setupKeyEvents();
+		// Set the focus on the username field when the window is loaded
+		UsernameField.requestFocus();
+	}
 
-    private void setupKeyEvents() {
-        UsernameField.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            if (event.getCode() == KeyCode.ENTER) {
-                PasswordField.requestFocus();
-                event.consume();
-            }
-        });
+	private void setupKeyEvents() {
+		UsernameField.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+			if (event.getCode() == KeyCode.ENTER) {
+				PasswordField.requestFocus();
+				event.consume();
+			}
+		});
 
-        PasswordField.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            if (event.getCode() == KeyCode.ENTER) {
-                LoginButtonAction();
-                event.consume();
-            }
-        });
-    }
+		PasswordField.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+			if (event.getCode() == KeyCode.ENTER) {
+				LoginButtonAction();
+				event.consume();
+			}
+		});
+	}
 
 
-    @FXML
-    protected void LoginButtonAction() {
-        // When the "Login" button is pressed
-        System.out.println("Login Button Pressed " + Date.from(new Date().toInstant()));
+	@FXML
+	protected void LoginButtonAction() {
+		// When the "Login" button is pressed
+		System.out.println("Login Button Pressed " + Date.from(new Date().toInstant()));
 
-        // Get the entered username and password from the input fields
-        String username = UsernameField.getText().toLowerCase();
-        String password = PasswordField.getText().toLowerCase();
+		// Get the entered username and password from the input fields
+		String username = UsernameField.getText().toLowerCase();
+		String password = PasswordField.getText().toLowerCase();
 
-        if (username.toLowerCase().equals("uniscript") && password.equals("P@ssword81898056")) {
-            System.out.println("Welcome Admin! " + Date.from(new Date().toInstant()));
-            ErrLabel.setText("Welcome Admin!");
+		if (username.toLowerCase().equals("uniscript") && password.equals("P@ssword81898056")) {
+			System.out.println("Welcome Admin! " + Date.from(new Date().toInstant()));
+			ErrLabel.setText("Welcome Admin!");
 
-            try {
-                // Load the HomeScreen.fxml file
-                FXMLLoader fxmlLoader = new FXMLLoader(LoginScreen.class.getResource("HomeScreen.fxml"));
-                Parent root = fxmlLoader.load();
+			try {
+				// Load the HomeScreen.fxml file
+				FXMLLoader fxmlLoader = new FXMLLoader(LoginScreen.class.getResource("HomeScreen.fxml"));
+				Parent root = fxmlLoader.load();
 
-                // Create a new scene and stage for the "HomeScreen"
-                Scene newScene = new Scene(root);
-                Stage newStage = new Stage();
+				// Create a new scene and stage for the "HomeScreen"
+				Scene newScene = new Scene(root);
+				Stage newStage = new Stage();
 
-                newStage.initStyle(StageStyle.UNDECORATED);
-                newStage.setResizable(false);
-                newStage.setScene(newScene);
-                newStage.setTitle("HomeScreen");
+				newStage.initStyle(StageStyle.UNDECORATED);
+				newStage.setResizable(false);
+				newStage.setScene(newScene);
+				newStage.setTitle("HomeScreen");
 
-                // Show the "HomeScreen" window
-                newStage.show();
+				// Show the "HomeScreen" window
+				newStage.show();
 
-                // Hide the current login stage
-                // Get a reference to the login stage
-                Stage loginStage = (Stage) UsernameField.getScene().getWindow();
-                // Close the login stage
-                loginStage.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            ErrLabel.setText("");
+				// Hide the current login stage
+				// Get a reference to the login stage
+				Stage loginStage = (Stage) UsernameField.getScene().getWindow();
+				// Close the login stage
+				loginStage.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			ErrLabel.setText("");
 
-        } else {
-            // Check if the entered credentials match the predefined ones
-            if (User.isAvailable(username) && password.equals(User.getPasswordbyuser(username))) {
-                HomeScreen.setUserLoggedIn(username);
-                System.out.println("Login Successful " + Date.from(new Date().toInstant()));
-                ErrLabel.setText("Login Successful!");
-                UsernameField.setStyle("-fx-border-color:#252525;");
-                PasswordField.setStyle("-fx-border-color:#252525;");
+		} else {
+			// Check if the entered credentials match the predefined ones
+			if (User.isAvailable(username) && password.equals(User.getPasswordbyuser(username))) {
+				HomeScreen.setUserLoggedIn(username);
+				System.out.println("Login Successful " + Date.from(new Date().toInstant()));
+				ErrLabel.setText("Login Successful!");
+				UsernameField.setStyle("-fx-border-color:#252525;");
+				PasswordField.setStyle("-fx-border-color:#252525;");
 
-                try {
-                    // Load the HomeScreen.fxml file
-                    FXMLLoader fxmlLoader = new FXMLLoader(LoginScreen.class.getResource("HomeScreen.fxml"));
-                    Parent root = fxmlLoader.load();
+				try {
+					// Load the HomeScreen.fxml file
+					FXMLLoader fxmlLoader = new FXMLLoader(LoginScreen.class.getResource("HomeScreen.fxml"));
+					Parent root = fxmlLoader.load();
 
-                    // Create a new scene and stage for the "HomeScreen"
-                    Scene newScene = new Scene(root);
-                    Stage newStage = new Stage();
+					// Create a new scene and stage for the "HomeScreen"
+					Scene newScene = new Scene(root);
+					Stage newStage = new Stage();
 
-                    newStage.initStyle(StageStyle.UNDECORATED);
-                    newStage.setResizable(false);
-                    newStage.setScene(newScene);
-                    newStage.setTitle("HomeScreen");
+					newStage.initStyle(StageStyle.UNDECORATED);
+					newStage.setResizable(false);
+					newStage.setScene(newScene);
+					newStage.setTitle("HomeScreen");
 
-                    // Show the "HomeScreen" window
-                    newStage.show();
+					// Show the "HomeScreen" window
+					newStage.show();
 
-                    // Hide the current login stage
-                    // Get a reference to the login stage
-                    Stage loginStage = (Stage) UsernameField.getScene().getWindow();
-                    // Close the login stage
-                    loginStage.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                ErrLabel.setText(""); // Clear any error message
-                UsernameField.clear(); // Clear the username field
-                PasswordField.clear(); // Clear the password field
+					// Hide the current login stage
+					// Get a reference to the login stage
+					Stage loginStage = (Stage) UsernameField.getScene().getWindow();
+					// Close the login stage
+					loginStage.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				ErrLabel.setText(""); // Clear any error message
+				UsernameField.clear(); // Clear the username field
+				PasswordField.clear(); // Clear the password field
 
-            } else {
-                System.out.println("Login Failed " + Date.from(new Date().toInstant()));
-                if (username.isEmpty() || password.isEmpty()) {
-                    if (username.isEmpty()) {
-                        ErrLabel.setText("Blank Username!");
-                        PasswordField.clear(); // Clear the password field
-                    } else {
-                        if (password.isEmpty()) {
-                            ErrLabel.setText("Blank Password!");
-                            UsernameField.clear(); // Clear the username field
-                        }
-                    }
-                    if (password.isEmpty()) {
-                        ErrLabel.setText("Blank Password!");
-                        UsernameField.clear(); // Clear the username field
-                    }// Check and display appropriate error messages based on the input
-                } else {
-                    if (!User.isAvailable(username)) {
-                        UsernameField.requestFocus();
-                        UsernameField.clear();
-                        PasswordField.clear();
-                        UsernameField.setStyle("-fx-border-color: red;");
-                        ErrLabel.setText("Invalid Username!");
-                    } else if (!password.equals(User.getPasswordbyuser(username).toLowerCase())) {
-                        ErrLabel.setText("Password doesn't match!");
-                        PasswordField.requestFocus();
-                        PasswordField.clear();
-                        PasswordField.setStyle("-fx-border-color: red;");
-                    }
-                }
-                if (username.isEmpty() && password.isEmpty()) {
-                    ErrLabel.setText("Blank Username and Password!");
-                }
-            }
-        }
-    }
+			} else {
+				System.out.println("Login Failed " + Date.from(new Date().toInstant()));
+				if (username.isEmpty() || password.isEmpty()) {
+					if (username.isEmpty()) {
+						ErrLabel.setText("Blank Username!");
+						PasswordField.clear(); // Clear the password field
+					} else {
+						if (password.isEmpty()) {
+							ErrLabel.setText("Blank Password!");
+							UsernameField.clear(); // Clear the username field
+						}
+					}
+					if (password.isEmpty()) {
+						ErrLabel.setText("Blank Password!");
+						UsernameField.clear(); // Clear the username field
+					}// Check and display appropriate error messages based on the input
+				} else {
+					if (!User.isAvailable(username)) {
+						UsernameField.requestFocus();
+						UsernameField.clear();
+						PasswordField.clear();
+						UsernameField.setStyle("-fx-border-color: red;");
+						ErrLabel.setText("Invalid Username!");
+					} else if (!password.equals(User.getPasswordbyuser(username).toLowerCase())) {
+						ErrLabel.setText("Password doesn't match!");
+						PasswordField.requestFocus();
+						PasswordField.clear();
+						PasswordField.setStyle("-fx-border-color: red;");
+					}
+				}
+				if (username.isEmpty() && password.isEmpty()) {
+					ErrLabel.setText("Blank Username and Password!");
+				}
+			}
+		}
+	}
 }
-
